c603987d7aa2f6058909ae6c4b38233760940a21
Adam Stajer <stajeradam757@gmail.com>
2023-02-26 13:42:33 +0100

    Add basic boost system. x2, x5, x10 boost

--- Script/script.js
+++ Script/script.js
@@ -1,8 +1,39 @@
 const appleIcon = document.querySelector('.apple__img');
 let scoreInfo = document.querySelector('.apple__score');
+let boostInfo = 1;
+const boost = (boost) => {
+    for(let i=0;i<boost;i++) {
+        scoreInfo.innerHTML++;
 
+    }
+}
 appleIcon.addEventListener('click', () => {
-    scoreInfo.innerHTML++;
+    boost(boostInfo);
     console.log("add point - work");
+})
 
+const x2Boost = document.querySelector('.x2boost');
+const x5Boost = document.querySelector('.x5boost');
+const x10Boost = document.querySelector('.x10boost');
+
+x2Boost.addEventListener('click', () => {
+    if(scoreInfo.innerHTML >= 20) {
+        boostInfo = 2;
+        scoreInfo.innerHTML -= 10;
+        x2Boost.style.display = 'none';
+    }
+})
+x5Boost.addEventListener('click', () => {
+    if(scoreInfo.innerHTML >= 50) {
+        boostInfo = 5;
+        scoreInfo.innerHTML -= 25;
+        x5Boost.style.display = 'none';
+    }
+})
+x10Boost.addEventListener('click', () => {
+    if(scoreInfo.innerHTML >= 200) {
+        boostInfo = 10;
+        scoreInfo.innerHTML -= 100;
+        x10Boost.style.display = 'none';
+    }
 })
