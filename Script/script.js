4c13e0ad1bc2db61b4871fee24f655c797494f32
Adam Stajer <stajeradam757@gmail.com>
2023-02-26 14:04:49 +0100

    Add basic autoClick system. 10,25,50 points/sec autoclick

--- Script/script.js
+++ Script/script.js
@@ -1,11 +1,18 @@
 const appleIcon = document.querySelector('.apple__img');
 let scoreInfo = document.querySelector('.apple__score');
 let boostInfo = 1;
+let autoclickInfo = 0;
 const boost = (boost) => {
     for(let i=0;i<boost;i++) {
         scoreInfo.innerHTML++;
-
     }
+    console.log('boost - work')
+}
+function autoClick(autoclick) {
+    for(let i=0;i<autoclick;i++) {
+        scoreInfo.innerHTML++;
+    }
+    console.log('autoclick - work')
 }
 appleIcon.addEventListener('click', () => {
     boost(boostInfo);
@@ -35,5 +42,34 @@ x10Boost.addEventListener('click', () => {
         boostInfo = 10;
         scoreInfo.innerHTML -= 100;
         x10Boost.style.display = 'none';
+    }
+})
+
+const x10autoclick = document.querySelector('.x10autoclick');
+const x25autoclick = document.querySelector('.x25autoclick');
+const x50autoclick = document.querySelector('.x50autoclick');
+
+x10autoclick.addEventListener('click', () => {
+    if(scoreInfo.innerHTML >= 250) {
+        autoclickInfo = 10;
+        scoreInfo.innerHTML -= 125;
+        x10autoclick.style.display = 'none';
+        setInterval(() => {
+            autoClick(autoclickInfo);
+        }, 1000);
+    }
+})
+x25autoclick.addEventListener('click', () => {
+    if(scoreInfo.innerHTML >= 1000) {
+        autoclickInfo = 25;
+        scoreInfo.innerHTML -= 500;
+        x25autoclick.style.display = 'none';
+    }
+})
+x50autoclick.addEventListener('click', () => {
+    if(scoreInfo.innerHTML >= 2500) {
+        autoclickInfo = 50;
+        scoreInfo.innerHTML -= 2000;
+        x50autoclick.style.display = 'none';
     }
 })
