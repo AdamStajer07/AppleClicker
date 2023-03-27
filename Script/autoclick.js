f497bbe1aedeca05e659e062e96d3b649573b36e
Adam Stajer <126243157+AdamStajer07@users.noreply.github.com>
2023-03-27 20:19:29 +0200

    Add 3 new autoclick boost
    Add 3 new autoclick boost (1000/2000/5000 apples/sec)

--- Script/autoclick.js
+++ Script/autoclick.js
@@ -4,6 +4,9 @@ const x50autoclick = document.querySelector('.x50autoclick');
 const x100autoclick = document.querySelector('.x100autoclick');
 const x250autoclick = document.querySelector('.x250autoclick');
 const x500autoclick = document.querySelector('.x500autoclick');
+const x1000autoclick = document.querySelector('.x1000autoclick');
+const x2000autoclick = document.querySelector('.x2000autoclick');
+const x5000autoclick = document.querySelector('.x5000autoclick');
 
 const autoClickArray = [
     x10autoclick,
@@ -12,6 +15,9 @@ const autoClickArray = [
     x100autoclick,
     x250autoclick,
     x500autoclick,
+    x1000autoclick,
+    x2000autoclick,
+    x5000autoclick
 ]
 
 x10autoclick.style.display = 'block';
@@ -74,7 +80,7 @@ autoClickArray[3].addEventListener('click', () => {
         scoreInfo.innerHTML -= 3000;
         for(let i=0;i<=3;i++){
             autoClickArray[i].style.display = 'none';
-            // autoClickArray[i+3].style.display = 'block';
+            autoClickArray[i+3].style.display = 'block';
         }
         rotate();
     }
@@ -90,7 +96,7 @@ autoClickArray[4].addEventListener('click', () => {
         scoreInfo.innerHTML -= 7500;
         for(let i=0;i<=4;i++){
             autoClickArray[i].style.display = 'none';
-            // autoClickArray[i+3].style.display = 'block';
+            autoClickArray[i+3].style.display = 'block';
         }
         rotate();
     }
@@ -105,6 +111,54 @@ autoClickArray[5].addEventListener('click', () => {
         autoclickInfo = 500;
         scoreInfo.innerHTML -= 20000;
         for(let i=0;i<=5;i++){
+            autoClickArray[i].style.display = 'none';
+            autoClickArray[i+3].style.display = 'block';
+        }
+        rotate();
+    }
+})
+autoClickArray[6].addEventListener('click', () => {
+    if(scoreInfo.innerHTML >= 75000) {
+        if(autoclickInfo != 10 && autoclickInfo != 25 && autoclickInfo != 50 && autoclickInfo != 100 && autoclickInfo != 250 && autoclickInfo != 500) {
+            setInterval(() => {
+                autoClick(autoclickInfo);
+            }, 1000);
+        }
+        autoclickInfo = 1000;
+        scoreInfo.innerHTML -= 60000;
+        for(let i=0;i<=6;i++){
+            autoClickArray[i].style.display = 'none';
+            // autoClickArray[i+3].style.display = 'block';
+        }
+        rotate();
+    }
+})
+autoClickArray[7].addEventListener('click', () => {
+    if(scoreInfo.innerHTML >= 200000) {
+        if(autoclickInfo != 10 && autoclickInfo != 25 && autoclickInfo != 50 && autoclickInfo != 100 && autoclickInfo != 250 && autoclickInfo != 500 && autoclickInfo != 1000) {
+            setInterval(() => {
+                autoClick(autoclickInfo);
+            }, 1000);
+        }
+        autoclickInfo = 2000;
+        scoreInfo.innerHTML -= 150000;
+        for(let i=0;i<=7;i++){
+            autoClickArray[i].style.display = 'none';
+            // autoClickArray[i+3].style.display = 'block';
+        }
+        rotate();
+    }
+})
+autoClickArray[8].addEventListener('click', () => {
+    if(scoreInfo.innerHTML >= 750000) {
+        if(autoclickInfo != 10 && autoclickInfo != 25 && autoclickInfo != 50 && autoclickInfo != 100 && autoclickInfo != 250 && autoclickInfo != 500 && autoclickInfo != 1000 && autoclickInfo != 2000) {
+            setInterval(() => {
+                autoClick(autoclickInfo);
+            }, 1000);
+        }
+        autoclickInfo = 3000;
+        scoreInfo.innerHTML -= 600000;
+        for(let i=0;i<=8;i++){
             autoClickArray[i].style.display = 'none';
             // autoClickArray[i+3].style.display = 'block';
         }
